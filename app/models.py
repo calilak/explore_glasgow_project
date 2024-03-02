49cadd880bd01d93a6408e7587a92ecb3ea84998
Ollie Livingston <2756026L@student.gla.ac.uk>
2024-03-02 14:21:19 +0000

    Defining a User model for the users profile

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# ollie working on this
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # The additional attributes we wish to include.
+    followers = models.IntegerField(default=0)
+    # count of public plans
+    # average stars calculated
+    # count of reviews
+    account_created = models.DateTimeField(auto_now_add=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    
+    def __str__(self):
+        return self.user.username
