d3beed83fe0feeca839bbfacb5aae620cba6ba3f
Liaoo7 <2709935l@student.gla.ac.uk>
2024-03-04 14:53:03 +0000

    basic Login and Register

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,8 @@ app_name = "app"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('restricted/', views.restricted, name='restricted'),
+    path('logout/', views.user_logout, name='logout'),
 ]
