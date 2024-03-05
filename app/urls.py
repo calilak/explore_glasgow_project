638e91d00ccca4baa6291e8650b41fe9ae491506
calilak <2771804c@student.gla.ac.uk>
2024-03-05 14:36:27 +0000

    Added map HTML page

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ app_name = "app"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("map/", views.map)
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index page of Explore Glasgow.")
+
+def map(request):
+    return render(request, "app/map.html")
