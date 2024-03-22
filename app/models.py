8c74d4a7d57b0d09477d6aced3ac8ed8e83e73be
revilowc <olivercwchan@gmail.com>
2024-03-22 00:33:09 +0000

    Merged with up to date main for unit testing files, models.py and tests.py

--- app/models.py
+++ app/models.py
@@ -6,7 +6,6 @@ from django.db.models import Avg, Count
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.utils import timezone
-from django.template.defaultfilters import slugify
 
 
 class UserProfile(models.Model):
@@ -56,14 +55,8 @@ class Tag(models.Model):
 class Place(models.Model):
     location = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
-    slug = models.SlugField()
-    img_ref = models.CharField(max_length=100, default="")
-    categories = models.ManyToManyField(Category)
-    tags = models.ManyToManyField(Tag)
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Place, self).save(*args, **kwargs)
+    categories = models.ManyToManyField(Category, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return self.name
@@ -74,8 +67,8 @@ class Event(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     location = models.ForeignKey(Place, on_delete=models.CASCADE)
-    categories = models.ManyToManyField(Category)
-    tags = models.ManyToManyField(Tag)
+    categories = models.ManyToManyField(Category, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return self.title
@@ -109,25 +102,18 @@ class Plan(models.Model):
         schedule = json.loads(self.schedule)
         schedule.append({'type': 'event', 'data': event.id})
         self.schedule = json.dumps(schedule)
+        self.events.add(event)
         self.save()
 
     def add_activity(self, activity, start_time):
         schedule = json.loads(self.schedule)
         schedule.append({'type': 'activity', 'data': activity.id, 'start_time': str(start_time)})
         self.schedule = json.dumps(schedule)
+        self.activities.add(activity)
         self.save()
 
     def get_schedule(self):
-        schedule_details = []
-        for item in self.schedule:
-            if item['type'] == 'event':
-                event = Event.objects.get(id=item['data'])
-                schedule_details.append({'type': 'event', 'data': event})
-            elif item['type'] == 'activity':
-                activity = Activity.objects.get(id=item['data'])
-                start_time = item['start_time']
-                schedule_details.append({'type': 'activity', 'data': activity, 'start_time': start_time})
-        return schedule_details
+        return self.schedule
 
     def __str__(self):
         return f"{self.user.username}'s Plan on {self.date}"
